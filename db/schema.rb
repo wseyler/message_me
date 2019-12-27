6b479aef1281239191d35ab596fcb2cf3d25ca0a
William E. Seyler <wseyler@pentaho.com>
2019-12-27 09:18:59 -0500

    Created Message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_27_134248) do
+ActiveRecord::Schema.define(version: 2019_12_27_135835) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
