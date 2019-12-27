718dbde9b4adceb43cfa516d050da7748dcdd1a6
William E. Seyler <wseyler@pentaho.com>
2019-12-27 08:55:58 -0500

    Added User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "wseyler", password: "password")
+User.create(username: "sseyler", password: "password")
+User.create(username: "lseyler", password: "password")
+User.create(username: "scottseyler", password: "password")
+User.create(username: "stephanieseyler", password: "password")
